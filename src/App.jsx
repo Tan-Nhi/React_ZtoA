1a3db1e376267c1e0be930387e56a00f25d512a9
Nguyễn Tấn Nhi <144986314+Tan-Nhi@users.noreply.github.com>
2025-05-05 02:44:12 +0700

    video 34

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,30 @@
-import { useState } from 'react'
+import './component/todo/todo.css'
+import TodoData from './component/todo/TodoData'
+import TodoNew from './component/todo/TodoNew'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import MyComponent from './component/learn/MyComponent'
-import {SecondComponent, ThirdComponent} from "./component/learn/SecondComponent"
-
 
 const App = () => {
-  const [count, setCount] = useState(0)
 
+  const crocodile = 'Crocodile T1';
+  const age = 25;
+  const data = {
+    address: "hanoi",
+    country: "Vietnam"
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="todo-container">
+      <div className="todo-title">Todo List</div>
+      <TodoNew />
+      <TodoData
+        name={crocodile}
+        age={age}
+        data={data}
+      />
+      <div className="todo-image">
+        <img src={reactLogo} alt="" className='logo' />
       </div>
-      <h1>Hello world</h1>
-      <MyComponent/>
-      <SecondComponent/>
-      <ThirdComponent/>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ src/component/todo/TodoData.jsx
@@ -0,0 +1,24 @@
+const TodoData = (props) => {
+    // props là 1 biến object chứa tất cả các thuộc tính được truyền từ component cha xuống
+    // {
+    //     name: "Crocodile T1",
+    //     age: 25,
+    //     data: {}
+    // }
+    const { name, age, data } = props; //cách 1  ưu tiên dùng
+    
+    // const name = props.name; //cách 2
+    // const age = props.age; //cách 2  
+    // const data = props.data; //cách 2
+
+    console.log("check props", props);
+    return (
+        <div className='todo-data'>
+            <div>My name is {name}</div>
+            <div>Learning React</div>
+            <div>Watching Youtube</div>
+        </div>
+    );
+}
+
+export default TodoData; 
