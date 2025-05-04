fc4333456b0e7e71efbb669976c4542f0a27a024
Nguyễn Tấn Nhi <144986314+Tan-Nhi@users.noreply.github.com>
2025-05-04 22:46:47 +0700

    video 31

--- src/component/learn/MyComponent.jsx
+++ src/component/learn/MyComponent.jsx
@@ -4,9 +4,21 @@
 // <> </> là Fragment  
 import './style.css'
 const MyComponent = () => {
+    // const tannhi = "Ark One"; string
+    // const tannhi = 25; number
+    // const tannhi = true; boolean 
+    // const tannhi = undefined; undefined
+    // const tannhi = null; null
+    const tannhi = [1, 2, 3]; // array
+    // const tannhi = {
+    //     name: "Ark One",
+    //     age: 25
+    // }; object
+
     return (
         <>
-            <div> Tan Nhi Update </div>
+            <div> Tan Nhi {JSON.stringify(tannhi)} </div>
+            <div>{console.log("eric1")}</div>
             <div className="child"
                 style={{ fontSize: "30px" }}>Child </div>
         </>
