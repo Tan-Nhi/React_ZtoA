88dd4b904c542977a053b287679e73d60215aaff
Nguyễn Tấn Nhi <144986314+Tan-Nhi@users.noreply.github.com>
2025-05-05 16:16:44 +0700

    video 36

--- src/component/todo/TodoNew.jsx
+++ src/component/todo/TodoNew.jsx
@@ -2,10 +2,25 @@ const TodoNew = (props) => {
     console.log(props);
     const { addnewtodo } = props;
     // addnewtodo("Crocodile T1");
+
+    //fire 
+    const handleClcick = () => {
+        alert(`Click me`)
+    }
+
+    const handleOnchange = (name) => {
+        console.log("Onchange", name);
+
+    }
+
     return (
         <div className="todo-new">
-            <input type="text" />
-            <button >Add</button>
+            <input type="text"
+                onChange={(event) => handleOnchange(event.target.value)} />
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClcick}  //tham chiếu func
+            >Add</button>
         </div>
     );
 }
